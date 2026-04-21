ca62106a83ea4e04c267c8161a77f373b4862dad
alammobilepk <itsshani5656@gmail.com>
2026-04-21 17:17:44 +0500

    Professional Vercel Config and Server Export

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/foundation.dart';
 
 class ApiConfig {
-  // AGAR CHROME HAI TOH LOCALHOST, AGAR PHONE HAI TOH APNA IP ADDRESS LIKHEIN
-  static String get baseUrl {
-    if (kIsWeb) {
-      return "http://localhost:3000/api/auth";
-    } else {
-      return "http://192.168.100.11:3000/api/auth"; // <--- Yahan apna IPv4 address likhein
-    }
-  }
+  // Ab aapka backend Vercel par live hai, toh IP address ki zaroorat nahi
+  static const String baseUrl = "https://alam-store.vercel.app/api/auth";
+
   static String get sendOtp => "$baseUrl/send-otp";
   static String get register => "$baseUrl/register";
 }
